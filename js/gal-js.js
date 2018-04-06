8be9cdf482e6a76f44b4d5746c4146d77f34613f
Roman <romanproweb@gmail.com>
2018-04-06 16:58:34 +0300

    create project

---
+++ js/gal-js.js
@@ -0,0 +1,4 @@
+// $('.gal_slider').slick({
+// 	autoplay:true,
+// 	autoplaySpeed:2000,
+// });
---
+++ js/my-popup.js
@@ -0,0 +1,12 @@
+$('#popupbutton').fancybox({
+        'padding': 37,
+        'overlayOpacity': 0.87,
+        'overlayColor': '#fff',
+        'transitionIn': 'none',
+        'transitionOut': 'none',
+        'titlePosition': 'inside',
+        'centerOnScroll': true,
+        'maxWidth': 249,
+        'minHeight': 280,
+ 
+    });
